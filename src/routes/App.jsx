1398e705f92982178191ecf886d400bde967e14f
diazj <jorjorf@gmail.com>
2022-04-18 18:46:36 -0400

    Navegación entre rutas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layaout from "../containers/Layaout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 const App = () => {
@@ -11,13 +13,13 @@ const App = () => {
         <BrowserRouter>
 
             <Routes>
-                <Layaout>
+               
                     <Route exact path="/" element={ <Home />} />
                     <Route exact path="/login" element={
                         <Login /> } />
                     <Route exact path="/recovery-password" element={ <RecoveryPassword  /> } />
-                    <Route element={ <NotFound />} />
-                </Layaout>
+                    <Route path="*" element={ <NotFound />} />
+               
             </Routes>
             
         </BrowserRouter>
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath:'/'
     },
     mode: 'development',
     resolve: {
@@ -68,5 +69,6 @@ module.exports = {
         
         compress: true,
         port: 3005,
+        historyApiFallback: true, 
     }
 }
