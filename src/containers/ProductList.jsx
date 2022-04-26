2aab0e9a5319dbf92b3d9d880d681dd7f893880b
diazj <jorjorf@gmail.com>
2022-04-26 19:09:39 -0400

    Custom Hooks para la tienda

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProductItem from '@components/ProductItem';
+import useGetProducts from '@hooks/useGetProducts';
 import '@styles/ProductList.scss';
-import axios from 'axios';
+
 
 const API = 'https://api.escuelajs.co/api/v1/products?limit=5&offset=1'
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(async() => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
-
+	const products = useGetProducts(API);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
 				{products.map(product => (
-					<ProductItem />
+					<ProductItem product={product} key={product.id} />
 				))}
 				
 			</div>
